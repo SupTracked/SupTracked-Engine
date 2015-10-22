5dd471f7300662391554ea3d076b3628ed314e6e
jkingsman <jack.kingsman@gmail.com>
2015-10-22 13:51:37 -0700

    rework structure

---
+++ test/authorization.js
@@ -0,0 +1,57 @@
+process.env.NODE_ENV = 'test';
+
+var request = require('supertest');
+require = require('really-need');
+
+describe('authorization', function() {
+  var server;
+
+  beforeEach(function() {
+    server = require('../bin/www', {
+      bustCache: true
+    });
+  });
+
+  afterEach(function(done) {
+    server.close(done);
+  });
+
+  it('requests authorization', function testUserAuthReq(done) {
+    request(server)
+      .get('/user')
+      .expect(401, done);
+  });
+
+  it('rejects invalid users', function testUserAuthInvalid(done) {
+    request(server)
+      .get('/user')
+      .auth('idontexist', 'lol')
+      .expect(401, done);
+  });
+
+  it('rejects invalid passwords', function testUserAuthBadPass(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .get('/user')
+          .auth('myusername', 'NotMyPassword')
+          .expect(401, done);
+      });
+  });
+
+  it('accepts valid users', function testUserAuthValid(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .get('/user')
+          .auth('myusername', 'MyPassword')
+          .expect(200, done);
+      });
+  });
+});
---
+++ test/experience.js
@@ -0,0 +1,179 @@
+process.env.NODE_ENV = 'test';
+
+var request = require('supertest');
+require = require('really-need');
+
+describe('experience', function() {
+  var server;
+
+  beforeEach(function() {
+    server = require('../bin/www', {
+      bustCache: true
+    });
+  });
+
+  afterEach(function(done) {
+    server.close(done);
+  });
+
+  it('requests authorization', function testExperienceAuthReq(done) {
+    request(server)
+      .post('/experience')
+      .expect(401, done);
+  });
+
+  it('denies missing JSON', function testExperienceMissingJSON(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .expect(400, done);
+      });
+  });
+
+  it('denies missing fields', function testExperienceMissing(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .expect(400, done);
+      });
+  });
+
+  it('denies invalid timestamp', function testExperienceBadTimestamp(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": "not a date"}')
+          .expect(400, done);
+      });
+  });
+
+  it('creates a valid experience', function testExperienceCreation(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .expect(201, done);
+      });
+  });
+
+  it('retrieves a valid experience', function testExperienceRetrieval(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function(){
+            request(server)
+              .get('/experience')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"id": 1}')
+              .expect(200, {
+                "date": 1445543583,
+                "id": 1,
+                "location": "My Location",
+                "notes": null,
+                "owner": 1,
+                "panicmsg": null,
+                "rating_id": null,
+                "title": "My Title",
+                "ttime": null
+              }, done);
+          });
+      });
+  });
+
+  it('updates a valid experience', function testExperienceUpdate(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // create a new experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function(){
+            // edit that experience
+            request(server)
+              .put('/experience')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"id": 1, "title": "My New Title"}')
+              .end(function(){
+                // request and check that it was updated
+                request(server)
+                  .get('/experience')
+                  .auth('myusername', 'MyPassword')
+                  .set('Content-Type', 'application/json')
+                  .send('{"id": 1}')
+                  .expect(200, {
+                    "date": 1445543583,
+                    "id": 1,
+                    "location": "My Location",
+                    "notes": null,
+                    "owner": 1,
+                    "panicmsg": null,
+                    "rating_id": null,
+                    "title": "My New Title",
+                    "ttime": null
+                  }, done);
+              });
+          });
+      });
+  });
+
+  it('denies experience update with an invalid field', function testExperienceUpdateInvalid(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // create a new experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function(){
+            // edit that experience
+            request(server)
+              .put('/experience')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"id": 1, "notafield": "value"}')
+              .expect(400, done);
+          });
+      });
+  });
+});
