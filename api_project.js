e6517b856bbe93b27539598965c81921302af17e
Jack Kingsman <jack.kingsman@gmail.com>
2015-10-22 21:21:34 -0700

    initial docs

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "SupTracked API",
+  "description": "API to track dietary supplements and nootropic consumption",
+  "title": "SupTracked API",
+  "version": "0.0.1",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-10-23T04:13:25.187Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
