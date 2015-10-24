e24a5b51d151fb399c7826656a941f79b6ffca6b
jkingsman <jack.kingsman@gmail.com>
2015-10-23 19:47:06 -0700

    add consumption reading and tests

--- routes/consumption.js
+++ routes/consumption.js
@@ -177,36 +177,32 @@ router.post('/', function(req, res, next) {
 });
 
 /**
- * @api {get} /experience Get a JSON object of an experience
- * @apiName GetExperience
- * @apiGroup Experience
+ * @api {get} /consumption Get a JSON object of an consumption
+ * @apiName GetConsumption
+ * @apiGroup Consumption
  *
- * @apiParam {Number} id  ID of the desired experience
+ * @apiParam {Number} id  id of the desired consumption
  *
  * @apiPermission ValidUserBasicAuthRequired
  *
  * @apiSuccess {Number} id  id of the experience
- * @apiSuccess {Number} date  date of the experience
- * @apiSuccess {Number} ttime  id of the consumption for which T-0:00 time format is based off
- * @apiSuccess {String} title  title of the experience
- * @apiSuccess {String} location  location of the experience
- * @apiSuccess {String} notes  notes for the experience
- * @apiSuccess {String} panicmsg  user's panic message for the created experience
- * @apiSuccess {Number} rating_id  rating of general experience quality
- * @apiSuccess {Number} owner  id of the owner of the experience
+ * @apiSuccess {Number} date  Unix timestamp of the date and time of the consumption
+ * @apiSuccess {Number} count  numerical quantity as measured by the drug's unit
+ * @apiSuccess {Number} experience_id  ID of the experience the consumption is part of
+ * @apiSuccess {Number} drug_id  ID of the drug consumed
+ * @apiSuccess {Number} method_id  ID of the method used to consume the drug
+ * @apiSuccess {Number} owner  id of the owner of the consumption
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *     {
+          "id": 3,
  *        "date": 1445543583,
- *        "id": 1,
- *        "location": "My Location",
- *        "notes": "This is great.",
- *        "owner": 1,
- *        "panicmsg": "Oh snap help me!",
- *        "rating_id": 3,
- *        "title": "Great Time",
- *        "ttime": null
+ *        "count": 3,
+ *        "experience_id": "1",
+ *        "drug_id": 4,
+ *        "method_id": 1,
+ *        "owner": 1
  *     }
  *
  * @apiError missingID id was not provided
@@ -214,7 +210,7 @@ router.post('/', function(req, res, next) {
  * @apiErrorExample Error-Response:
  *     HTTP/1.1 400 Bad Request
  *     {
- *       "experience": "id must be provided"
+ *       "consumption": "id must be provided"
  *     }
  *
  * @apiError noRecords no results found for the given ID
@@ -227,26 +223,26 @@ router.get('/', function(req, res, next) {
   if (req.body === undefined || !("id" in req.body) || isNaN(req.body.id)) {
     res.setHeader('Content-Type', 'application/json');
     res.status(400).send(JSON.stringify({
-      experience: "id must be provided"
+      consumption: "id must be provided"
     }));
     return;
   }
 
   // get the entry
-  db.get("SELECT * FROM experiences WHERE id = $id AND owner = $owner", {
+  db.get("SELECT * FROM consumptions WHERE id = $id AND owner = $owner", {
     $id: req.body.id,
     $owner: req.supID
   }, function(err, row) {
     if (err) {
       res.setHeader('Content-Type', 'application/json');
       res.status(400).send(JSON.stringify({
-        experience: err
+        consumption: err
       }));
       return;
     }
 
     // no rows returned; nothing for that ID
-    if (row == []) {
+    if (row === undefined) {
       res.setHeader('Content-Type', 'application/json');
       res.status(404).send();
       return;
@@ -255,212 +251,6 @@ router.get('/', function(req, res, next) {
     // return the experience
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(JSON.stringify(row));
-  });
-});
-
-/**
- * @api {put} /experience Update an experience
- * @apiName UpdateExperience
- * @apiGroup Experience
- *
- * @apiParam {Number} id  id of the experience
- * @apiParam {Number} [date]  date of the experience
- * @apiParam {Number} [ttime]  id of the consumption for which T-0:00 time format is based off
- * @apiParam {String} [title]  title of the experience
- * @apiParam {String} [location]  location of the experience
- * @apiParam {String} [notes]  notes for the experience
- * @apiParam {String} [panicmsg]  user's panic message for the created experience
- * @apiParam {Number} [rating_id]  rating of general experience quality
- *
- * @apiPermission ValidUserBasicAuthRequired
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *
- * @apiError noFields no fields to set were provided
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 400 Bad Request
- *     {
- *       "experience": "no fields provided"
- *     }
- *
- * @apiError illegalField a field to update was send that is not permitted (must be in above list)
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 400 Bad Request
- *     {
- *       "experience": "custom field requested that is not permitted"
- *     }
- */
-router.put('/', function(req, res, next) {
-  var permittedFields = ['date', 'location', 'notes', 'panicmsg', 'rating_id', 'title', 'ttime', 'id'];
-
-  //no fields were provided
-  if (Object.keys(req.body).length === 0 || req.body === undefined) {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(400).send(JSON.stringify({
-      experience: "no fields provided"
-    }));
-    return;
-  }
-
-  if (Object.keys(req.body).every(function(field) {
-      return permittedFields.indexOf(field) >= 0;
-    })) {
-    // all the keys of the request body (AKA all requested fields) are allowed; let them pass
-
-    // assemble the query
-    var columns = Object.keys(req.body).join(', ');
-    var updateVals = [];
-    var dataArray = {};
-
-    // set the column1 = value1, etc. for the update
-    Object.keys(req.body).forEach(function(columnName) {
-      updateVals.push(columnName + ' = $' + columnName);
-    });
-
-    var query = 'UPDATE experiences SET ' + updateVals.join(', ') + ' WHERE id = $expid AND owner = $owner';
-    dataArray.$owner = req.supID;
-
-    // loop through each key and build the JSON object of bindings for sqlite
-    Object.keys(req.body).forEach(function(columnName) {
-      dataArray["$" + columnName] = req.body[columnName];
-    });
-
-    // add the experience ID
-    dataArray.$expid = req.body.id;
-
-    db.run(query, dataArray, function(err) {
-      if (err) {
-        res.status(500).send();
-        return;
-      }
-
-      // all done. loaded and ready.
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).send();
-    });
-  } else {
-    // they tried to send an unsupported key; kick 'em out
-    res.setHeader('Content-Type', 'application/json');
-    res.status(400).send(JSON.stringify({
-      experience: "custom field requested that is not permitted"
-    }));
-  }
-});
-
-/**
- * @api {get} /experience/search Retrieve an array of experiences that match the provided criteria
- * @apiName SearchExperience
- * @apiGroup Experience
- *
- * @apiParam {Number} [startdate]  Unix timestamp of beginning of date range to select
- * @apiParam {Number} [enddate]  Unix timestamp of end of date range to select
- * @apiParam {String} [title]  experiences where this string is contained in the title will be retrieved
- * @apiParam {String} [location]  experiences where this string is contained in the location field will be retrieved
- * @apiParam {String} [notes]  experiences where this string is contained in the notes field will be retrieved
- * @apiParam {Number} [rating_id]  experiences with this rating will be retrieved
- * @apiParam {Number} [limit]  only return this number of rows
- * @apiParam {Number} [offset]  offset the returned number of rows by this amount (requires limit)
- *
- * @apiPermission ValidUserBasicAuthRequired
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *
- * @apiError noResults no experiences match the provided criteris
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 404 Not Found Bad Request
- *
- */
-router.get('/search', function(req, res, next) {
-  // get our limits and offset
-  var limitOffset = "";
-
-  // start assembling the query
-  var queryData = {};
-
-  // base and owner
-  var query = "SELECT * FROM experiences WHERE owner = $owner";
-  queryData.$owner = req.supID;
-
-  if (req.body !== undefined) {
-    if ("limit" in req.body) {
-      if (parseInt(req.body.limit)) {
-        // we have a parseable int
-        limitOffset += " LIMIT " + parseInt(req.body.limit);
-      }
-    }
-
-    if ("offset" in req.body) {
-      if (parseInt(req.body.offset)) {
-        // we have a parseable int
-        limitOffset += "," + parseInt(req.body.offset);
-      }
-    }
-
-    // get date range
-    if ("startdate" in req.body && "enddate" in req.body) {
-      // we have date parameters
-      query += " AND date BETWEEN $startdate AND $enddate";
-      queryData.$startdate = req.body.startdate;
-      queryData.$enddate = req.body.enddate;
-    }
-
-    // get rating
-    if ("rating_id" in req.body) {
-      // we have date parameters
-      query += " AND rating_id = $rating_id";
-      queryData.$rating_id = req.body.rating_id;
-    }
-
-    // get location
-    if ("location" in req.body) {
-      // we have date parameters
-      query += " AND location LIKE '%' || $location || '%'";
-      queryData.$location = req.body.location;
-    }
-
-    // get notes
-    if ("notes" in req.body) {
-      // we have date parameters
-      query += " AND notes LIKE '%' || $notes || '%'";
-      queryData.$notes = req.body.notes;
-    }
-
-    // get title
-    if ("title" in req.body) {
-      // we have date parameters
-      query += " AND title LIKE '%' || $title || '%'";
-      queryData.$title = req.body.title;
-    }
-
-    // slap the limit and offset on the enddate
-    query += limitOffset;
-  }
-
-  // get the entry
-  db.all(query, queryData, function(err, rows) {
-    if (err) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(400).send(JSON.stringify({
-        experience: err
-      }));
-      return;
-    }
-
-    // no rows returned
-    if (rows.length === 0) {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(404).send();
-      return;
-    }
-
-    // return the experience
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).send(JSON.stringify(rows));
   });
 });
 
--- test/consumption.js
+++ test/consumption.js
@@ -202,6 +202,209 @@ describe('consumption', function() {
       });
   });
 
+  it('retrieves a valid consumption', function testConsumptionCreation(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // make an experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function() {
+            // make a drug
+            request(server)
+              .post('/drug')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"name": "Phenylpiracetam",' +
+                '"unit": "mg",' +
+                '"notes": "Phenylpiracetam is a phenylated analog of the drug piracetam.",' +
+                '"classification": "AMPA modulator",' +
+                '"family": "*racetam",' +
+                '"rarity": "Common"' +
+                '}')
+              .end(function() {
+                request(server)
+                  .post('/method')
+                  .auth('myusername', 'MyPassword')
+                  .set('Content-Type', 'application/json')
+                  .send('{"name": "Oral",' +
+                    '"icon": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="' +
+                    '}')
+                  .end(function() {
+                    // add consumption
+                    request(server)
+                      .post('/consumption')
+                      .auth('myusername', 'MyPassword')
+                      .set('Content-Type', 'application/json')
+                      .send('{"count": 2, "experience_id": 1, "date": 1445648036, "drug_id": 1, "method_id": 1}')
+                      .end(function(){
+                        request(server)
+                          .get('/consumption')
+                          .auth('myusername', 'MyPassword')
+                          .set('Content-Type', 'application/json')
+                          .send('{"id": 1}')
+                          .expect(200, {
+                            "count": 2,
+                            "date": "1445648036",
+                            "drug_id": 1,
+                            "experience_id": 1,
+                            "id": 1,
+                            "method_id": 1,
+                            "owner": 1
+                          }, done);
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+  it('denies retrieval with missing JSON', function testConsumptionMissingJSON(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .get('/consumption')
+          .auth('myusername', 'MyPassword')
+          .expect(400, {
+            "consumption": "id must be provided"
+          }, done);
+      });
+  });
+
+  it('denies retrieval with missing id and no record', function testConsumptionMissingIDNoRecord(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        request(server)
+          .get('/consumption')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .expect(400, {
+            "consumption": "id must be provided"
+          }, done);
+      });
+  });
+
+  it('denies retrieval with missing id and a record', function testConsumptionMissingIDWithRecord(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // make an experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function() {
+            // make a drug
+            request(server)
+              .post('/drug')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"name": "Phenylpiracetam",' +
+                '"unit": "mg",' +
+                '"notes": "Phenylpiracetam is a phenylated analog of the drug piracetam.",' +
+                '"classification": "AMPA modulator",' +
+                '"family": "*racetam",' +
+                '"rarity": "Common"' +
+                '}')
+              .end(function() {
+                request(server)
+                  .post('/method')
+                  .auth('myusername', 'MyPassword')
+                  .set('Content-Type', 'application/json')
+                  .send('{"name": "Oral",' +
+                    '"icon": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="' +
+                    '}')
+                  .end(function() {
+                    // add consumption
+                    request(server)
+                      .post('/consumption')
+                      .auth('myusername', 'MyPassword')
+                      .set('Content-Type', 'application/json')
+                      .send('{"count": 2, "experience_id": 1, "date": 1445648036, "drug_id": 1, "method_id": 1}')
+                      .end(function(){
+                        request(server)
+                          .get('/consumption')
+                          .auth('myusername', 'MyPassword')
+                          .set('Content-Type', 'application/json')
+                          .expect(400, {
+                            "consumption": "id must be provided"
+                          }, done);
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+  it('404s on a nonexistent ID', function testConsumptionNoRecordForID(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // make an experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function() {
+            // make a drug
+            request(server)
+              .post('/drug')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"name": "Phenylpiracetam",' +
+                '"unit": "mg",' +
+                '"notes": "Phenylpiracetam is a phenylated analog of the drug piracetam.",' +
+                '"classification": "AMPA modulator",' +
+                '"family": "*racetam",' +
+                '"rarity": "Common"' +
+                '}')
+              .end(function() {
+                request(server)
+                  .post('/method')
+                  .auth('myusername', 'MyPassword')
+                  .set('Content-Type', 'application/json')
+                  .send('{"name": "Oral",' +
+                    '"icon": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="' +
+                    '}')
+                  .end(function() {
+                    // add consumption
+                    request(server)
+                      .post('/consumption')
+                      .auth('myusername', 'MyPassword')
+                      .set('Content-Type', 'application/json')
+                      .send('{"count": 2, "experience_id": 1, "date": 1445648036, "drug_id": 1, "method_id": 1}')
+                      .end(function(){
+                        request(server)
+                          .get('/consumption')
+                          .auth('myusername', 'MyPassword')
+                          .set('Content-Type', 'application/json')
+                          .send('{"id": 2}')
+                          .expect(404, done);
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+
   //
   // it('creates a valid experience', function testExperienceCreation(done) {
   //   request(server)
