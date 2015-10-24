bbd3f00dff89a1430a20738f4c3d697a7e0fdd84
Jack Kingsman <jack.kingsman@gmail.com>
2015-10-24 04:07:17 -0700

    remove cruft and add comment

--- test/consumption.js
+++ test/consumption.js
@@ -1037,6 +1037,142 @@ describe('consumption', function() {
       });
   });
 
+  it('deletes a friend', function testConsumptionDeleteFriend(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // make an experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function() {
+            // make a drug
+            request(server)
+              .post('/drug')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"name": "Phenylpiracetam",' +
+                '"unit": "mg",' +
+                '"notes": "Phenylpiracetam is a phenylated analog of the drug piracetam.",' +
+                '"classification": "AMPA modulator",' +
+                '"family": "*racetam",' +
+                '"rarity": "Common"' +
+                '}')
+              .end(function() {
+                // make a method
+                request(server)
+                  .post('/method')
+                  .auth('myusername', 'MyPassword')
+                  .set('Content-Type', 'application/json')
+                  .send('{"name": "Oral",' +
+                    '"icon": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="' +
+                    '}')
+                  .end(function() {
+                    // add consumption
+                    request(server)
+                      .post('/consumption')
+                      .auth('myusername', 'MyPassword')
+                      .set('Content-Type', 'application/json')
+                      .send('{"count": 2, "experience_id": 1, "date": 1445648036, "location": "San Juan", "drug_id": 1, "method_id": 1}')
+                      .end(function() {
+                        request(server)
+                          .delete('/consumption/friend')
+                          .auth('myusername', 'MyPassword')
+                          .set('Content-Type', 'application/json')
+                          .send('{"id": 1}')
+                          .expect(200, done);
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+  it('verifies deleting a friend', function testConsumptionDeleteFriendVerify(done) {
+    request(server)
+      .post('/register')
+      .set('Content-Type', 'application/json')
+      .send('{"username": "myusername", "password": "MyPassword"}')
+      .end(function() {
+        // make an experience
+        request(server)
+          .post('/experience')
+          .auth('myusername', 'MyPassword')
+          .set('Content-Type', 'application/json')
+          .send('{"title": "My Title", "location": "My Location", "date": 1445543583}')
+          .end(function() {
+            // make a drug
+            request(server)
+              .post('/drug')
+              .auth('myusername', 'MyPassword')
+              .set('Content-Type', 'application/json')
+              .send('{"name": "Phenylpiracetam",' +
+                '"unit": "mg",' +
+                '"notes": "Phenylpiracetam is a phenylated analog of the drug piracetam.",' +
+                '"classification": "AMPA modulator",' +
+                '"family": "*racetam",' +
+                '"rarity": "Common"' +
+                '}')
+              .end(function() {
+                // make a method
+                request(server)
+                  .post('/method')
+                  .auth('myusername', 'MyPassword')
+                  .set('Content-Type', 'application/json')
+                  .send('{"name": "Oral",' +
+                    '"icon": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs="' +
+                    '}')
+                  .end(function() {
+                    // add consumption
+                    request(server)
+                      .post('/consumption')
+                      .auth('myusername', 'MyPassword')
+                      .set('Content-Type', 'application/json')
+                      .send('{"count": 2, "experience_id": 1, "date": 1445648036, "location": "San Juan", "drug_id": 1, "method_id": 1}')
+                      .end(function() {
+                        // add friend
+                        request(server)
+                          .post('/consumption/friend')
+                          .auth('myusername', 'MyPassword')
+                          .set('Content-Type', 'application/json')
+                          .send('{"consumption_id": 1, "name": "John Smith"}')
+                          .end(function() {
+                            // remove friend
+                            request(server)
+                              .delete('/consumption/friend')
+                              .auth('myusername', 'MyPassword')
+                              .set('Content-Type', 'application/json')
+                              .send('{"id": 1}')
+                              .end(function() {
+                                request(server)
+                                  .get('/consumption')
+                                  .auth('myusername', 'MyPassword')
+                                  .set('Content-Type', 'application/json')
+                                  .send('{"id": 1}')
+                                  .expect(200, {
+                                    "count": 2,
+                                    "date": "1445648036",
+                                    "drug_id": 1,
+                                    "experience_id": 1,
+                                    "friends": [],
+                                    "id": 1,
+                                    "location": "San Juan",
+                                    "method_id": 1,
+                                    "owner": 1
+                                  }, done);
+                              });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+
   it('returns a list of unique friends', function testAllFriends(done) {
     request(server)
       .post('/register')
