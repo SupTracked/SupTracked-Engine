a9b83a27a4b7b525206acbb4a84918f4437bab59
Jack Kingsman <jack.kingsman@gmail.com>
2015-10-24 18:17:02 -0700

    update docs

--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2015-10-24T07:17:21.376Z",
+    "time": "2015-10-25T01:12:56.590Z",
     "url": "http://apidocjs.com",
     "version": "0.13.1"
   }
