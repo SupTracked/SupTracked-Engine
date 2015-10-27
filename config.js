c8a0ea1414552ce4665d26b23ee98311e4d52ff3
Jack Kingsman <jack.kingsman@gmail.com>
2015-10-27 16:11:07 -0700

    add options for test db and media

--- config.js
+++ config.js
@@ -5,5 +5,10 @@ config.general.port = 3000;
 
 config.db = {};
 config.db.location = "data/db/suptracked.db";
+config.db.test_location = ":memory:";
+
+config.media = {};
+config.media.location = "data/media/";
+config.media.test_location = "data/media_test/"; // this location will be created if it doesn't exist and deleted after testing
 
 module.exports = config;
